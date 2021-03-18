ed32c3d27b36b2eb1b3386daee9f61413b28e0fa
sibasish.mohanty <sibasish.mohanty@goodera-blr.com>
2021-03-18 17:55:41 +0530

    tailwind and custom font added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Home = (): JSX.Element => {
 	return (
 		<>
-			<h1>Hello World</h1>
-			<p>
+			<h1 className='flex items-center justify-between mb-4 text-6xl font-bold'>
+				Hello World
+			</h1>
+			<p className='p-2'>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
 				blanditiis delectus quisquam, quaerat at, reprehenderit ullam ipsum
 				aliquid quibusdam consectetur officia esse! Numquam repellat cupiditate
