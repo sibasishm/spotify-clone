bc9767882c0abf1feec4e440f0d820ef919ca190
sibasish.mohanty <sibasish.mohanty@goodera-blr.com>
2021-03-18 15:19:16 +0530

    ts, jest, eslint, pretteir init

---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Button = (): JSX.Element => {
+	return <button>Click Me</button>;
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = (): JSX.Element => {
+	return (
+		<>
+			<h1>Hello World</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+				blanditiis delectus quisquam, quaerat at, reprehenderit ullam ipsum
+				aliquid quibusdam consectetur officia esse! Numquam repellat cupiditate
+				ut corporis nemo debitis soluta!
+			</p>
+		</>
+	);
+};
+
+export default Home;
