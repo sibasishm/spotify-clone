2e86c13a41a5e08432e73363e80412840db388b1
Sibasish Mohanty <sibasish.mohanty@goodera.com>
2021-03-20 19:05:04 +0530

    warnings supressed for the time being

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
